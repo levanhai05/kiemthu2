a5b7fa2d14bdf57a3ed0ab4c02c8d4ee79a7b453
levanhai05 <152960125+levanhai05@users.noreply.github.com>
2025-05-31 06:14:18 +0700

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', routes);
+app.use(routes);
 
 app.use((req, res, next) => {
   return next(Boom.notFound('This route does not exist.'));
